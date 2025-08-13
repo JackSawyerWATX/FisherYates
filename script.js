bdfff597c1e2824cc0254aba78f5275bd3a8081d
Jack Sawyer <contact@jonathanfausset.com>
2025-08-13 11:23:21 -0500

    Additions

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function generatePermutations(arr) {
   }
   
   // Usage example:
-  const nums = [1, 2, 3, 4, 5];
+  const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const permutations = generatePermutations(nums);
   
   // Log all the permutations to the console
